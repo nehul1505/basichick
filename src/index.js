ff54a7bc7d46c40e3879ad399c3250f29cb00cc8
Nehul Don <nehuldon@ASUS-TUF-Gaming-A15-FA507NU-FA577NU-170b81c3.localdomain>
2024-04-21 02:24:57 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
-import connectDB from './db/index.js';
+import connectDB from './db/index.js'
+import { app } from "./app.js";
 
 dotenv.config({
     path: '../.env'
 })
 
 connectDB()
+.then( () => {
+    app.on("error",(error) => {
+        console.error("ERROR IS:", error)
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is listning on PORT: ${process.env.PORT}`);
+    })}) 
+    .catch ((err) => {
+        console.log("ERROR in connecting to the Databse :", err);
+
+    })
 
 
 
