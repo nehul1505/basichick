7822ebf4c537bbc6151a66d98fcc6e132e54f625
Nehul Don <nehuldon@ASUS-TUF-Gaming-A15-FA507NU-FA577NU-170b81c3.localdomain>
2024-08-10 22:14:53 +0530

    add comment,like, playlist and tweet models

---
+++ src/models/playlist.model.js
@@ -0,0 +1,27 @@
+import mongoose, {Schema} from mongoose
+
+const playlistSchema = new Schema({
+name: {
+    type: String, 
+    required: true
+},
+description: {
+    type: String,
+    required: true
+},
+videos:[
+    {
+        type: Schema.types.ObjectId,
+        ref: "Video"
+    }
+] ,
+owner :{
+    type: Schema.Types.ObjectId,
+    ref: "User"
+}
+
+
+
+},{timestamps: true})
+
+export const Playlist = mongoose.model("Playlist", playlistSchema)
